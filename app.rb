aee839bd2233f4f4ebbe6b3c26614273526f5f8e
AJOsmaston <dajoxx@gmail.com>
2021-10-07 13:07:44 +0100

    extracted global variables, replaced with session

--- app.rb
+++ app.rb
@@ -14,24 +14,24 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @player_1 = $player_1
-    @player_2 = $player_2
+    @player_1_name = session[:player_1].name
+    @player_2_name = session[:player_2].name
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb(:play)
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    session[:player_1] = Player.new(params[:player_1_name])
+    session[:player_2] = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @player_1 = $player_1
-    @player_2 = $player_2
+    @player_1_name = session[:player_1].name
+    @player_2_name = session[:player_2].name
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     @player_1.attack(@player_2)
     erb(:attack)
   end
